d720e2347f3252ebce039a7c69b0d017a687e636
Dzianis Prudnikau <dzianis_prudnikau@epam.com>
2022-10-13 13:14:25 +0200

    Task 3 implementation.

---
+++ product-service/data/products.js
@@ -0,0 +1,44 @@
+export const products = [
+    {
+        id: 1,
+        price: 10,
+        author: "Jane Rollason",
+        title: "Haunted Britain",
+        description: "Haunted Britain",
+    },
+    {
+        id: 2,
+        price: 20,
+        author: "Kathy Burke",
+        title: "Breaking the Chain",
+        description: "Breaking the Chain",
+    },
+    {
+        id: 3,
+        price: 15,
+        author: "John Grogan",
+        title: "Marley and Me",
+        description: "Marley and Me",
+    },
+    {
+        id: 4,
+        price: 32,
+        author: "Jerome K-Jerome",
+        title: "Three men in a boat",
+        description: "Three men in a boat",
+    },
+    {
+        id: 5,
+        price: 5,
+        author: "Leonardo Da Vinci",
+        title: "Clarke Georgia",
+        description: "Clarke Georgia",
+    },
+    {
+        id: 6,
+        price: 25,
+        author: "John Galsworthy",
+        title: "The Forsyte Saga",
+        description: "The Forsyte Saga",
+    },
+];
