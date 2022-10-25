ebb2f60e7e115e95ddb08e624f603b21938f274c
kitminsk <kitminsk@gmail.com>
2022-10-25 12:13:33 +0200

    Task 4 implementation

---
+++ product-service/data/stocks.js
@@ -0,0 +1,26 @@
+export const stocks = [
+    {
+        product_id: "7e7d00ad-0396-429b-b896-02bba2243efa",
+        count: 100,
+    },
+    {
+        product_id: "d1cbbced-65d8-4725-a69c-4b6cb695770b",
+        count: 68,
+    },
+    {
+        product_id: "65d5b7ce-1109-4a74-be0f-bf1590ffebe9",
+        count: 84,
+    },
+    {
+        product_id: "bba7e24d-0ad9-4fac-af85-cc9e9cacc42c",
+        count: 99,
+    },
+    {
+        product_id: "b4e6d272-a892-4c6b-985c-13f44720911a",
+        count: 76,
+    },
+    {
+        product_id: "8e1ab278-47e3-4bdc-bea9-b71ae6754e00",
+        count: 111,
+    },
+];
--- product-service/handlers/getProductsList.js
+++ product-service/handlers/getProductsList.js
@@ -1,13 +1,38 @@
 import { cors } from "../data/cors.js";
-import { products } from "../data/products.js";
+import AWS from "aws-sdk";
+
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+const scan = async(params) => {
+    const response = await ddb.scan(params).promise();
+    return response.Items;
+};
+
+const productTableParams = {
+    TableName: process.env.TABLE_PRODUCTS
+}
+
+const stockTableParams = {
+    TableName: process.env.TABLE_STOCKS
+}
 
 export async function getProductsList(event) {
     console.log(`event: ${JSON.stringify(event)}`);
 
+    const products = await scan(productTableParams);
+    const stocks = await scan(stockTableParams);
+
+    const result = products.map((product) => {
+        const stock = stocks.find((st) => st.product_id == product.id);
+        if (stock) {
+            product.count = stock.count
+        }
+        return product;
+    });
+
     return {
         statusCode: 200,
         headers: { ...cors },
-        body: JSON.stringify(products),
+        body: JSON.stringify(result),
     };
-
 }
